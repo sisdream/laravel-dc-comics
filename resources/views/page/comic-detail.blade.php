874cbacaf5ffef5e2dfb6504368762dce7d95ebf
sisdream <sissipascale2@gmail.com>
2024-03-25 19:28:39 +0100

    feat: creato comic-create

--- resources/views/page/comic-detail.blade.php
+++ resources/views/page/comic-detail.blade.php
@@ -4,15 +4,20 @@
 
 @section('main-content')
 <main>
-    <span class="fumetti py-2 px-3">CURRENT SERIES</span>
+    <span class="fumetti py-3 px-3">CURRENT SERIES</span>
     <div class="row">
-        <div class="comic w-25">
-            <h2 class="mt-2 text-uppercase">{{$comic['title']}}</h2>
-            <div class="card-img">
+        <h2 class="mt-5 text-uppercase text-light">{{$comic['title']}}</h2>
+        <div class="d-flex my-3 gap-5">
+            <div class="card-comic-img">
                 <img src="{{$comic['thumb']}}" alt="fumetto">
             </div>
-            <p>{{$comic['description']}}</p>
-
+            <p class="text-light d-flex align-items-center">{{$comic['description']}}</p>
+        </div>
+        <div class="text-light d-flex gap-5 align-items-center justify-content-center" >
+             <div>Price: {{$comic['price']}}</div>
+             <div>Series: {{$comic['series']}}</div>
+             <div>Sale Date: {{$comic['sale_date']}}</div>
+             <div>Type: {{$comic['type']}}</div>
         </div>
     </div>
     <div class="d-flex justify-content-center my-3">
---
+++ resources/views/page/comics-create.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.app')
+
+
+
+@section('main-content')
+    <section>
+        <div class="container">
+            <h1>Crea nuovo fumetto</h1>
+
+            <form action="">
+                ...
+            </form>
+        </div>
+    </section>
+@endsection
