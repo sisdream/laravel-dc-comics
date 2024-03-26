d19d5584bf00f9ae25bdc317c92972fd710e68ec
sisdream <sissipascale2@gmail.com>
2024-03-26 17:27:11 +0100

    fix: aggiusti vari

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'title',
+        'thumb',
+        "description",
+        "price",
+        "series",
+        "sale_date",
+        "type",
+    ];
 }
