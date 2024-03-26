a6e23e1719bf8de36e6b5a740520841f7d2d7038
sisdream <sissipascale2@gmail.com>
2024-03-26 15:31:35 +0100

    feat: creato form

--- resources/views/page/comics-create.blade.php
+++ resources/views/page/comics-create.blade.php
@@ -1,15 +1,19 @@
 @extends('layout.app')
 
-
-
 @section('main-content')
     <section>
         <div class="container">
             <h1>Crea nuovo fumetto</h1>
 
-            <form action="">
-                ...
+            <form action="{{ route('comics.store') }}" method="POST">
+                @csrf
+                <label for="title">Titolo</label>
+                <input type="text" id="title" name="title">
+                <label for="description">Descrizione</label>
+                <textarea id="description" name="description"></textarea>
+                <button type="submit">Salva</button>
             </form>
         </div>
     </section>
 @endsection
+
