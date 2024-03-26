9146e6d1b21fa0b0154e520b0b75bbd5f168fe45
sisdream <sissipascale2@gmail.com>
2024-03-26 16:58:41 +0100

    feat: metodo store

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -37,7 +37,20 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-       //
+       
+        $data = $request->all();
+
+        $comic = new Comic;
+        $comic->title = $data["title"];
+        $comic->description = $data["description"];
+        $comic->thumb = $data["thumb"];
+        $comic->price = $data["price"];
+        $comic->series = $data["series"];
+        $comic->sale_date = $data["sale_date"];
+        $comic->type = $data["type"];
+        $comic->save();
+
+        return view("page.comic-detail", compact("comic"));
     }
 
     /**
